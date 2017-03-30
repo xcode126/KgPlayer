2c6d96587d6559c5ffb3cd296eab2a658fb36df1
xcode126 <xcode126@126.com>
2017-03-30 14:38:16 +0800

    KgPlayer

--- app/src/main/java/com/xcode126/kgplayer/MainActivity.java
+++ app/src/main/java/com/xcode126/kgplayer/MainActivity.java
@@ -3,6 +3,13 @@ package com.xcode126.kgplayer;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * 作者：sky
+ * 邮箱：xcode126@126.com
+ * QQ号：1397028339
+ * 公众号：走近程序员
+ * 作用：影音播放器
+ */
 public class MainActivity extends Activity {
 
     @Override
